447c03ee90fe8ff913ed923f98906a29533c4535
Einstein <przemekworld@gmail.com>
2014-05-02 11:22:18 +0200

    Nowy wygląd

--- Gazeta.pl/Presentation/Views/ShellWindow.xaml.cs
+++ Gazeta.pl/Presentation/Views/ShellWindow.xaml.cs
@@ -3,14 +3,16 @@ using System.Windows;
 using Gazeta.pl.Applications.Views;
 using System.Windows.Navigation;
 using System.Diagnostics;
+using MahApps.Metro.Controls;
 
 namespace Gazeta.pl.Presentation.Views
 {
     [Export(typeof(IShellView))]
-    public partial class ShellWindow : Window, IShellView
+    public partial class ShellWindow : MetroWindow, IShellView
     {
         public ShellWindow()
         {
+            
             InitializeComponent();
         }
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
@@ -19,5 +21,7 @@ namespace Gazeta.pl.Presentation.Views
             e.Handled = true;
         }
 
+
     }
+
 }
