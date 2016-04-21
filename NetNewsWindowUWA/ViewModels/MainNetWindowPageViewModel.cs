a3bd4b9c05155f23ecec25688bca0c630434aebe
Przemek <przemekworld@gmail.com>
2016-04-21 15:07:18 +0200

    Zmiana organizacji projektu

---
+++ NetNewsWindowUWA/ViewModels/MainNetWindowPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Windows.Mvvm;
+
+namespace NetNewsWindowUWA.ViewModels
+{
+    public class MainNetWindowPageViewModel : ViewModelBase
+    {
+        private string test;
+
+        public string onOff
+        {
+            get { return test; }
+            set { this.SetProperty<string>(ref test, value); }
+        }
+
+    }
+}
