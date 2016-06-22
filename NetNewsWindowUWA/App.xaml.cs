b645fddd92f0f9e20628eae7755736b3734c2100
Przemek <przemekworld@gmail.com>
2016-06-22 21:12:14 +0200

    Usunięcie wszystkiego

--- NetNewsWindowUWA/App.xaml.cs
+++ NetNewsWindowUWA/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Prism.Unity.Windows;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Prism.Mvvm;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -17,89 +20,64 @@ using Windows.UI.Xaml.Navigation;
 
 namespace NetNewsWindowUWA
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : PrismUnityApplication
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+        /// <summary>    
+        /// Initializes the singleton application object.  This is the first line of authored code    
+        /// executed, and as such is the logical equivalent of main() or WinMain().    
+        /// </summary>    
         public App()
         {
             this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            base.Suspending += OnSuspending;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
         {
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
-            Frame rootFrame = Window.Current.Content as Frame;
+            NavigationService.Navigate("Main", null);
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
+            Window.Current.Activate();
 
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
+            return Task.FromResult<object>(null);
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            RegisterTypes();
+            return base.OnInitializeAsync(args);
+        }
+
+        private void RegisterTypes()
+        {
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewModelTypeName = string.Format(System.Globalization.CultureInfo.InvariantCulture, "MyPrismApp.ViewModels.{0}ViewModel, MyPrismApp", viewType.Name);
+                var viewModelType = Type.GetType(viewModelTypeName);
+                return viewModelType;
+            });
+        }
+
+        /// <summary>    
+        /// Invoked when Navigation to a certain page fails    
+        /// </summary>    
+        /// <param name="sender">The Frame which failed navigation</param>    
+        /// <param name="e">Details about the navigation failure</param>    
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
+        /// <summary>    
+        /// Invoked when application execution is being suspended.  Application state is saved    
+        /// without knowing whether the application will be terminated or resumed with the contents    
+        /// of memory still intact.    
+        /// </summary>    
+        /// <param name="sender">The source of the suspend request.</param>    
+        /// <param name="e">Details about the suspend request.</param>    
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
+            //TODO: Save application state and stop any background activity    
             deferral.Complete();
         }
     }
